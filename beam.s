c0b0f579943b63b610feecb35748323ef54fc234
b192515 <111619982+b192515@users.noreply.github.com>
2022-09-26 09:33:09 +0530

    Add files via upload

---
+++ beam.s
@@ -0,0 +1,47 @@
+\documentclass{beamer}
+\usetheme{AnnArbor}
+\title{my \LaTeX}
+\author{sin}
+\date{\today}
+\begin{document}
+\begin{frame}
+\titlepage
+\end{frame}
+\begin{frame}
+
+\includegraphics[width=3cm]{my.jpg}
+\caption{shin}
+\end{frame}
+\begin{frame}
+ \tableofcontents
+
+ \section{pm}
+  \subsection{23khedgwd}
+ \section{oieudhfd}
+ \section{jdioheu}
+\end{frame}
+\begin{abstract}
+    content
+   \end{abstract}
+\begin{itemize}
+    \item pearl
+    \item gold
+    \end{itemize}
+\end{itemize}
+
+\begin{frame}{Text and image }
+ \begin{columns}
+  \column{0.4\textwidth}
+    w;ljdsoiwdghwyfduyiguhgiuySGEH\\QBLFNDAPFJDOFHDWUGFsgf\\dghjakfcsdfgwoudjfdhfdfd[pofew-0fueofjdalkf;lafpeiw8rerqtwyjpoiutyrewertyuiopokjhbgfdsedrftyguhijokp]
+ \column{0.6\textwidth}
+    \begin{figure}
+        \centering
+        \includegraphics[width=6cm]{my.png}
+        \caption{my}
+        \label{fig:my_label}
+    \end{figure}
+ \end{columns}
+ \end{frame}
+\end{document}
+
+
